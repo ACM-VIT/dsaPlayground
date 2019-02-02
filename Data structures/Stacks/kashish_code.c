f369a2afaa06e11e7bb0c26e815fa6f9eddb1155
Kashish Mittal <45570942+KASHISH2000@users.noreply.github.com>
2019-02-02 11:03:55 +0530

    implementation of stack

---
+++ Data structures/Stacks/kashish_code.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#define CAPACITY 7
+int top=-1;
+int stack[CAPACITY];
+void push(int);
+int isFull();
+int pop();
+int isEmpty();
+void display();
+void main()
+{
+int ch,e;
+while(1)
+{
+printf("1)insert\n");
+printf("2)delete\n");
+printf("3)display\n");
+printf("enter your choice:\n");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:printf("enter the element:\n");
+       scanf("%d",&e);
+       push(e);  
+       break;
+case 2:pop();
+break;
+case 3:display();
+break;
+default: printf("invalid input\n");
+}
+}
+}
+void push(int ele)
+{
+if(isFull())
+{
+printf("stack is full\n");
+}
+else
+{
+top++;
+stack[top]=ele;
+}
+}
+int isFull()
+{
+if(top==(CAPACITY-1))
+{return 1;}
+else
+{return 0;}
+}
+int pop()
+{ int ele;
+if(isEmpty())
+{printf("stack is empty\n");}
+else
+{ele=stack[top];
+top--;}
+return top;
+}
+int isEmpty()
+{
+if(top==-1)
+{return 1;}
+else
+{return 0;}
+}
+void display()
+{ int i;
+if(isEmpty())
+{printf("stack is empty\n");}
+else
+{
+for(i=0;i<=top;i++)
+{
+printf("%d\n",stack[i]);
+}
+}
+}
+
+
